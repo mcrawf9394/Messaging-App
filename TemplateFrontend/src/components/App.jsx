4edab6c1286486f45b3a020d5bff14506f12e466
mcrawf9394 <mjc9394@gmail.com>
2024-07-05 20:50:32 -0400

    fixed grammatical error

--- TemplateFrontend/src/components/App.jsx
+++ TemplateFrontend/src/components/App.jsx
@@ -24,7 +24,7 @@ function App() {
       <h1 className='sm:h-20 sticky top-0 text-center text-xl bg-blue-600 text-white mb-10 sm:mb-0 sm:text-6xl'>Messaging App</h1>
       <div className='w-10/12 h-4/5 mx-auto bg-white mt-2 sm:mt-5 rounded-lg'>
         <h2 className='sm:text-3xl sm:mb-10 text-center mx-5 border-b-4 border-solid border-black pt-4 pb-4'>This is a messaging app built for the Odin Project</h2>
-        <p className='text-sm md:text-base my-10 indent-6 mx-10'>On this application, you can create an account and send messages to other users. This project was made with the MERN stack and uses the passport strategy for jsonwebtokens to authenticate users. The project also uses socket.io to create a better user experience of the chatrooms. Socket.io does the heavy lifting when it comes to creating websockets so that the the user does not constantly have to refresh the page to view new messages.</p>
+        <p className='text-sm md:text-base my-10 indent-6 mx-10'>On this application, you can create an account and send messages to other users. This project was made with the MERN stack and uses the passport strategy for jsonwebtokens to authenticate users. The project also uses socket.io to create a better user experience of the chatrooms. Socket.io does the heavy lifting when it comes to creating websockets so that the user does not constantly have to refresh the page to view new messages.</p>
         <div className='w-full inline-flex justify-around mt-5 sm:mt-28'>
           <button className='md:h-20 md:w-40 transition duration-300 ease-in-out border-4 border-black border-solid sm:hover:scale-125 hover:bg-black hover:text-white' onClick={click => {
             click.preventDefault()
